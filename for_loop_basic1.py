543a5d9826c5e93398544f5b4468533c49592b85
Your Name <email@example.com>
2023-06-05 10:49:32 -0700

    finished

--- for_loop_basic1.py
+++ for_loop_basic1.py
@@ -1,6 +1,6 @@
 1.
 
-for i in range(150):
+for i in range(151):
     print(i)
 
 
@@ -15,19 +15,32 @@ for i in range(5,1000,5):
 for i in range(1,100):
     if i % 5 == 0:
         print('Coding')
-    elif i % 10 == 0:
+    if i % 10 == 0:
         print('Coding Dojo')
-    else:
-        print(i)
 
 
 4.
 
 sum = 0
-for i in range(0, 500000):
-    if i % 2 == 1:
-        sum += i
+for i in range(0, 500001, 3):
+    sum += i
     print(sum)
 
 
 5.
+
+def count_down_four():
+    num = 2018
+    while num > 0:
+        print(num)
+        num = num - 4
+
+
+6.
+
+def flex(low, high ,mult):
+    for i in range(low, high):
+        if i % mult == 0:
+            print(i)
+
+flex(2, 9, 3)
