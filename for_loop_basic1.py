b1db9780e0fced87ee7bd2cc36f2b1fa9323d49f
Your Name <email@example.com>
2023-06-05 10:35:59 -0700

    first commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,33 @@
+1.
+
+for i in range(150):
+    print(i)
+
+
+2.
+
+for i in range(5,1000,5):
+    print(i)
+
+
+3.
+
+for i in range(1,100):
+    if i % 5 == 0:
+        print('Coding')
+    elif i % 10 == 0:
+        print('Coding Dojo')
+    else:
+        print(i)
+
+
+4.
+
+sum = 0
+for i in range(0, 500000):
+    if i % 2 == 1:
+        sum += i
+    print(sum)
+
+
+5.
